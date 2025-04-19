4714bef80cd7f385193275e556c0e018ed0400ed
wizard-cloud <myfredm21@gmail.com>
2025-04-19 04:50:10 +0000

    actualizo archivo index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hola Mundo en WordPress</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+<body>
+    <h1><?php bloginfo('name'); ?></h1>
+    <p>¡Hola Mundo desde mi primer tema!</p>
+</body>
+</html>
